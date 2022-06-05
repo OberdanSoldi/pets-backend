bc790b09cad32cc7e558f69cfca96e89902ddb78
oberdansoldi <oberdan_soldi@outlook.com.br>
2022-06-05 18:59:04 -0300

    refactor: renomeando PetDto para PetResponseDto

--- src/main/java/br/com/adopt/petsbackend/service/PetService.java
+++ src/main/java/br/com/adopt/petsbackend/service/PetService.java
@@ -1,6 +1,6 @@
 package br.com.adopt.petsbackend.service;
 
-import br.com.adopt.petsbackend.domain.dto.PetDto;
+import br.com.adopt.petsbackend.domain.dto.PetResponseDto;
 import br.com.adopt.petsbackend.domain.entity.Pet;
 import br.com.adopt.petsbackend.repository.PetRepository;
 import br.com.adopt.petsbackend.service.adapter.PetMapper;
@@ -18,8 +18,8 @@ public class PetService {
 
     private final PetMapper petMapper;
 
-    public List<PetDto> findAll() {
+    public List<PetResponseDto> findAll() {
         final List<Pet> pets = petRepository.findAll();
-        return pets.stream().map(petMapper::toPetDto).collect(Collectors.toList());
+        return pets.stream().map(petMapper::toPetResponseDto).collect(Collectors.toList());
     }
 }
