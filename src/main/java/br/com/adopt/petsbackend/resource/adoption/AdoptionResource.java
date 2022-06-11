06b9b376d9e7debf1cbee17040f63a89068704e8
oberdansoldi <oberdan_soldi@outlook.com.br>
2022-06-10 23:09:12 -0300

    feat: Adicionando novos endpoints

--- src/main/java/br/com/adopt/petsbackend/resource/adoption/AdoptionResource.java
+++ src/main/java/br/com/adopt/petsbackend/resource/adoption/AdoptionResource.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/adoption")
@@ -15,8 +18,13 @@ public class AdoptionResource {
 
     private final AdoptionService adoptionService;
 
-    @PostMapping("/")
-    public ResponseEntity<AdoptionResponseDto> create(@RequestBody final AdoptionRequestDto adoptionRequestDto) {
+    @PostMapping
+    public ResponseEntity<AdoptionResponseDto> create(@RequestBody @Valid final AdoptionRequestDto adoptionRequestDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(adoptionService.create(adoptionRequestDto));
+    }
+
+    @GetMapping
+    public List<AdoptionResponseDto> findAll() {
+        return adoptionService.findAll();
     }
 }
--- src/main/java/br/com/adopt/petsbackend/resource/pet/PetResource.java
+++ src/main/java/br/com/adopt/petsbackend/resource/pet/PetResource.java
@@ -1,14 +1,15 @@
 package br.com.adopt.petsbackend.resource.pet;
 
+import br.com.adopt.petsbackend.domain.dto.PetRequestDto;
 import br.com.adopt.petsbackend.domain.dto.PetResponseDto;
 import br.com.adopt.petsbackend.service.PetService;
+import io.swagger.models.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -18,9 +19,14 @@ public class PetResource {
 
     private final PetService petService;
 
-    @GetMapping("/")
+    @GetMapping
     public ResponseEntity<List<PetResponseDto>> getAll() {
         return ResponseEntity.status(HttpStatus.OK).body(petService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<PetResponseDto> create(@RequestBody @Valid final PetRequestDto petRequestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(petService.create(petRequestDto));
     }
 
 }
